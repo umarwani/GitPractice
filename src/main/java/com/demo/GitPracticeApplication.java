1b15697c4ba912998bebd22805258a747b142b73
umar wani <umar wani@DESKTOP-GB4KBS8>
2024-05-18 12:08:18 +0530

    master: removed all methods

--- src/main/java/com/demo/GitPracticeApplication.java
+++ src/main/java/com/demo/GitPracticeApplication.java
@@ -8,46 +8,6 @@ public class GitPracticeApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GitPracticeApplication.class, args);
-
-		System.out.println("master: line1 added");
-
-		System.out.println("master: line2 added");
-		
-		System.out.println("feature: line1 added");
-		
-		System.out.println("feature: line2 added");
-		
-		System.out.println("master: line3 added");
-		
-	}
-	public void sampleMethodAdded() {
-		System.out.println("master: line1 added");
-
-		System.out.println("master: line2 added");
-		
-		System.out.println("feature: line1 added");
-		
-		System.out.println("feature: line2 added");
-		System.out.println("master: line1 added");
-
-		System.out.println("master: line2 added");
-		
-		System.out.println("feature: line1 added");
-		
-		System.out.println("feature: line2 added");
-		System.out.println("master: line1 added");
-
-		System.out.println("master: line2 added");
-		
-		System.out.println("feature: line1 added");
-		
-		System.out.println("feature: line2 added");
-		
-		System.out.println("master conflict line added");
 	}
 	
-	public void test1methd() {
-		
-		System.out.println("feature/test1: line1 added");
-	}
 }
